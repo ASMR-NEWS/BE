9a51386bd03759448055eb7524445793367fd6f0
JoonKyoLee <joonkyolee@gmail.com>
2025-02-25 20:36:48 +0900

    feat: Member 엔티티 생성

---
+++ src/main/java/com/neutral/newspaper/domain/member/Member.java
@@ -0,0 +1,31 @@
+package com.neutral.newspaper.domain.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Builder
+    public Member(Long memberId, String name, String email, String password) {
+        this.memberId = memberId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
