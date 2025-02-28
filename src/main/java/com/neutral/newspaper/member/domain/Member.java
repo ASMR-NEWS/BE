de374b0b1faf1b69a3b144e13c4422c21d8a5b05
JoonKyoLee <joonkyolee@gmail.com>
2025-02-28 23:45:18 +0900

    feat: Member 엔티티 필드에 @Column 어노테이션 추가 각 필드의 제약 조건을 명확히 하기 위해 추가

--- src/main/java/com/neutral/newspaper/member/domain/Member.java
+++ src/main/java/com/neutral/newspaper/member/domain/Member.java
@@ -1,9 +1,11 @@
 package com.neutral.newspaper.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,19 +13,24 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "members")
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     @Builder
-    public Member(Long memberId, String name, String email, String password) {
-        this.memberId = memberId;
+    public Member(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
