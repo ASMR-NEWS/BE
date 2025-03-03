a721d670286972a4d93a64f3ce2d4209948f2cfd
JoonKyoLee <joonkyolee@gmail.com>
2025-03-03 22:28:44 +0900

    feat: 비밀번호를 BCryptPasswordEncoder로 암호화한 후 저장하도록 변경

--- src/main/java/com/neutral/newspaper/member/service/MemberService.java
+++ src/main/java/com/neutral/newspaper/member/service/MemberService.java
@@ -3,15 +3,18 @@ package com.neutral.newspaper.member.service;
 import com.neutral.newspaper.member.MemberRepository;
 import com.neutral.newspaper.member.domain.Member;
 import com.neutral.newspaper.member.dto.MemberJoinRequestDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
@@ -24,10 +27,12 @@ public class MemberService {
             throw new IllegalArgumentException("비밀번호는 8~16자의 영어, 숫자, 특수기호(@$!%*?&)를 포함해야 합니다.");
         }
 
+        String encodedPassword = passwordEncoder.encode(joinRequest.getPassword());
+
         Member member = Member.builder()
                 .name(joinRequest.getName())
                 .email(joinRequest.getEmail())
-                .password(joinRequest.getPassword())
+                .password(encodedPassword)
                 .build();
 
         memberRepository.save(member);
