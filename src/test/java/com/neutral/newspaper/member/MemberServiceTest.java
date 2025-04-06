eed5a3f2350ac5bb01a30acea74a6918f9164cdb
JoonKyoLee <joonkyolee@gmail.com>
2025-04-06 20:23:16 +0900

    test: 로그인 테스트 및 회원가입 테스트 수정 - LoginRequestDto에 @AllArgsConstructor 추가 - 로그인 성공, 실패 케이스 테스트 추가 - 회원가입 실패 케이스 진행 시 오류 메시지 일치 확인

--- src/test/java/com/neutral/newspaper/member/MemberServiceTest.java
+++ src/test/java/com/neutral/newspaper/member/MemberServiceTest.java
@@ -2,8 +2,11 @@ package com.neutral.newspaper.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.neutral.newspaper.jwt.JwtToken;
 import com.neutral.newspaper.member.dto.JoinRequestDto;
+import com.neutral.newspaper.member.dto.LoginRequestDto;
 import com.neutral.newspaper.member.service.MemberService;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -55,7 +58,8 @@ public class MemberServiceTest {
 
             // then
             assertThatThrownBy(() -> memberService.registerMember(joinRequestDto2))
-                    .isInstanceOf(IllegalArgumentException.class);
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이미 존재하는 회원입니다.");
         }
 
         @Test
@@ -68,7 +72,8 @@ public class MemberServiceTest {
 
             // when, then
             assertThatThrownBy(() -> memberService.registerMember(joinRequestDto))
-                    .isInstanceOf(IllegalArgumentException.class);
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("비밀번호는 8~16자의 영어, 숫자, 특수기호(@$!%*?&)를 포함해야 합니다.");
         }
 
         @Test
@@ -81,7 +86,79 @@ public class MemberServiceTest {
 
             // when, then
             assertThatThrownBy(() -> memberService.registerMember(joinRequestDto))
-                    .isInstanceOf(IllegalArgumentException.class);
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("휴대폰 번호는 10~11자여야 합니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("로그인 테스트")
+    class loginTest {
+
+        @Test
+        @DisplayName("로그인 성공 케이스")
+        void successLogin() {
+            // given
+            JoinRequestDto joinRequestDto = new JoinRequestDto(
+                    "홍길동", "email@example.com", "TestPassword12!", "010-1234-5678", List.of("경제", "스포츠")
+            );
+            memberService.registerMember(joinRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto(
+                    "email@example.com", "TestPassword12!"
+            );
+
+            // when
+            JwtToken jwtToken = memberService.login(loginRequestDto);
+
+            // then
+            assertNotNull(jwtToken);
+        }
+
+        @Test
+        @DisplayName("로그인 실패 케이스: 존재하지 않는 회원")
+        void failLoginNotRegistered() {
+            // given
+            JoinRequestDto joinRequestDto = new JoinRequestDto(
+                    "홍길동", "email@example.com", "TestPassword12!", "010-1234-5678", List.of("경제", "스포츠")
+            );
+            memberService.registerMember(joinRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto(
+                    "fail@example.com", "TestPassword12!!"
+            );
+
+            // when
+            JwtToken jwtToken = memberService.login(loginRequestDto);
+
+            // then
+            assertNull(jwtToken);
+            assertThatThrownBy(() -> memberService.login(loginRequestDto))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("존재하지 않는 회원입니다.");
+        }
+
+        @Test
+        @DisplayName("로그인 실패 케이스: 비밀번호 불일치")
+        void failLoginPasswordMismatch() {
+            // given
+            JoinRequestDto joinRequestDto = new JoinRequestDto(
+                    "홍길동", "email@example.com", "TestPassword12!", "010-1234-5678", List.of("경제", "스포츠")
+            );
+            memberService.registerMember(joinRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto(
+                    "email@example.com", "TestPassword12!!"
+            );
+
+            // when
+            JwtToken jwtToken = memberService.login(loginRequestDto);
+
+            // then
+            assertNull(jwtToken);
+            assertThatThrownBy(() -> memberService.login(loginRequestDto))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("비밀번호가 잘못되었습니다.");
         }
     }
 }
