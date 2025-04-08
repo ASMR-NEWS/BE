23ddf8866bb7cd2af568c8715a53598cc6a95772
JoonKyoLee <joonkyolee@gmail.com>
2025-04-08 23:47:25 +0900

    refactor: @AllArgsConstructor 추가

--- src/main/java/com/neutral/newspaper/member/dto/VerifyCodeDto.java
+++ src/main/java/com/neutral/newspaper/member/dto/VerifyCodeDto.java
@@ -1,10 +1,12 @@
 package com.neutral.newspaper.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class VerifyCodeDto {
     private String email;
     private String verificationCode;
