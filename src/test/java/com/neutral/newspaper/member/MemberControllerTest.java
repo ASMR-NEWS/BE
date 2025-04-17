e3a8017338b42c02077f8ffc3fde0ac84542fe07
JoonKyoLee <joonkyolee@gmail.com>
2025-04-17 20:05:03 +0900

    refactor: MemberController 테스트 최소화 및 중복 제거 - MemberService에서 검증된 로직에 대한 중복 테스트 제거 - 컨트롤러 레이어에서 성공 케이스 및 상태 코드 흐름만 테스트 하도록 정리

--- src/test/java/com/neutral/newspaper/member/MemberControllerTest.java
+++ src/test/java/com/neutral/newspaper/member/MemberControllerTest.java
@@ -10,12 +10,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neutral.newspaper.global.CustomException;
 import com.neutral.newspaper.global.ErrorType;
+import com.neutral.newspaper.jwt.JwtToken;
 import com.neutral.newspaper.member.controller.MemberController;
 import com.neutral.newspaper.member.dto.JoinRequestDto;
+import com.neutral.newspaper.member.dto.LoginRequestDto;
 import com.neutral.newspaper.member.service.MemberService;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -39,7 +42,7 @@ public class MemberControllerTest {
 
     @WithMockUser
     @Test
-    @DisplayName("회원가입 성공")
+    @DisplayName("회원가입 요청 성공 시 200 반환")
     void successSignUp() throws Exception {
         JoinRequestDto joinRequest = new JoinRequestDto(
                 "홍길동", "email@example.com", "TestPassword12!", "010-1234-5678", List.of("경제", "스포츠")
@@ -52,56 +55,5 @@ public class MemberControllerTest {
                         .content(objectMapper.writeValueAsString(joinRequest))
                         .with(csrf()))
                 .andExpect(status().isOk());
-    }
-
-    @WithMockUser
-    @Test
-    @DisplayName("회원가입 실패 - 이메일 중복")
-    void failSignUpDuplicatedEmail() throws Exception {
-        JoinRequestDto joinRequest = new JoinRequestDto(
-                "홍길동", "email@example.com", "TestPassword12!", "010-1234-5678", List.of("경제", "스포츠")
-        );
-
-        when(memberService.registerMember(any(JoinRequestDto.class))).thenThrow(new CustomException(ErrorType.DUPLICATED_EMAIL));
-
-        mockMvc.perform(post("/member/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(joinRequest))
-                        .with(csrf()))
-                .andExpect(status().isConflict());
-    }
-
-    @WithMockUser
-    @Test
-    @DisplayName("회원가입 실패 - 비밀번호 형식 오류")
-    void failSignUpInvalidPassword() throws Exception {
-        JoinRequestDto joinRequest = new JoinRequestDto(
-                "홍길동", "email@example.com", "test", "010-1234-5678", List.of("경제", "스포츠")
-        );
-
-        when(memberService.registerMember(any(JoinRequestDto.class))).thenThrow(new CustomException(ErrorType.INVALID_PASSWORD_FORMAT));
-
-        mockMvc.perform(post("/member/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(joinRequest))
-                        .with(csrf()))
-                .andExpect(status().isBadRequest());
-    }
-
-    @WithMockUser
-    @Test
-    @DisplayName("회원가입 실패 - 휴대전화 번호 형식 오류")
-    void failSignUpInvalidPhoneNumber() throws Exception {
-        JoinRequestDto joinRequest = new JoinRequestDto(
-                "홍길동", "email@example.com", "TestPassword12!", "010-1234-56789", List.of("경제", "스포츠")
-        );
-
-        when(memberService.registerMember(any(JoinRequestDto.class))).thenThrow(new CustomException(ErrorType.INVALID_PHONE_NUMBER_FORMAT));
-
-        mockMvc.perform(post("/member/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(joinRequest))
-                        .with(csrf()))
-                .andExpect(status().isBadRequest());
     }
 }
